2405161ff8f9b13c4e9148d7f6022ff13c809d70
iron-island <ironislandmail@gmail.com>
2024-12-13 23:25:37 +0800

    Added initial delay trial, removed old key comparisons, added checks if button was pressed, and slightly adjusted x and y offsets but might still be incorrect

--- visualizations/2024/day13/source/main.c
+++ visualizations/2024/day13/source/main.c
@@ -5,6 +5,7 @@
 
 // Include the main libnx system header, for Switch development
 #include <switch.h>
+#include <math.h>
 
 // See also libnx pad.h / hid.h.
 
@@ -26,6 +27,31 @@
 #define CLAW_BOT  "/ \\"
 #define CLAW_ROW 29
 #define CLAW_COL 39
+
+#define PRIZE_MID "{*}"
+
+#define DELAY_MS 10
+
+void wait_delay(){
+    // Tick variables, based on:
+    //   https://github.com/switchbrew/switch-examples/tree/master/graphics/opengl/es2gears/source/main.c
+    double t;
+    static u64 origTicks = UINT64_MAX;
+
+    while (1) {
+        if (origTicks == UINT64_MAX)
+           origTicks = armGetSystemTick();
+        u64 ticksElapsed = armGetSystemTick() - origTicks;
+        //t = (ticksElapsed * 625 / 12) / 1000000000.0; // t in seconds?
+        t = (ticksElapsed * 625 / 12) / 1000000.0; // t in milliseconds?
+
+        if (t > DELAY_MS) {
+            origTicks = UINT64_MAX;
+            break;
+        }
+    }
+
+}
 
 // Main program entrypoint
 int main(int argc, char* argv[])
@@ -57,15 +83,19 @@ int main(int argc, char* argv[])
 
     u32 kDownOld = 0, kHeldOld = 0, kUpOld = 0; //In these variables there will be information about keys detected in the previous frame
 
-    int i;
+    int button_pressed = 0;
+
+    int i, x, y;
     int machine_num, total_machines;
     int ax, ay;
     int bx, by;
     int px, py;
+    int a_press, b_press;
     int curr_x, curr_y;
     int old_i, old_x, old_y;
-    int tokens, prizes;
+    int tokens, prizes, prize_caught;
     int grid_x_offset, grid_y_offset;
+    int diff_x, diff_y;
 
     // Screen grid variables
     // +10 for offsets, +1 on cols for null terminator
@@ -111,12 +141,23 @@ int main(int argc, char* argv[])
     // TODO:
     machine_num = 1;
     total_machines = 1;
+    // Original: 80 A presses, 40 B presses
     ax = 94;
     ay = 34;
     bx = 22;
     by = 67;
     px = 8400;
     py = 5400;
+    // Modified: 11 A presses, 8 B presses
+    a_press = 11;
+    b_press = 8;
+    ax = 5;//17;
+    ay = 3;//9;
+    bx = 4;//12;
+    by = 2;//8;
+    px = a_press*ax + b_press*bx;//8400;
+    py = a_press*ay + b_press*by;//5400;
+
     curr_x = 0;
     curr_y = 0;
     old_i = -1;
@@ -124,22 +165,24 @@ int main(int argc, char* argv[])
     old_y = 0;
     tokens = 0;
     prizes = 0;
+    prize_caught = 0;
     grid_x_offset = 0;
     grid_y_offset = 0;
 
     printf("\x1b[1;1H Advent of Code 2024 Day 13: Claw Contraption");
-    printf("\x1b[2;1H Instructions: Press A/B to move right/forward");
-    printf("\x1b[3;1H               Press X/Y to cancel A/B movements (no token refunds!)");
+    printf("\x1b[2;1H Instructions: Press A/B to move right/forward by some amount");
+    printf("\x1b[3;1H               Press X/Y to move left/backward by the same amount,");
+    printf("\x1b[4;1H                 but also costs tokens!");
 
-    printf("\x1b[4;1H   Machine %d of %d", machine_num, total_machines);
-    printf("\x1b[5;1H     Button A: X+%d, Y+%d", ax, ay);
-    printf("\x1b[6;1H     Button B: X+%d, Y+%d", bx, by);
-    printf("\x1b[7;1H     Prize: X=%d, Y=%d", px, py);
+    printf("\x1b[5;1H   Machine %d of %d", machine_num, total_machines);
+    printf("\x1b[6;1H     Button A: X+%d, Y+%d", ax, ay);
+    printf("\x1b[7;1H     Button B: X+%d, Y+%d", bx, by);
+    printf("\x1b[8;1H     Prize: X=%d, Y=%d", px, py);
 
-    printf("\x1b[9;1H   Current (X, Y) = (%d, %d)", curr_x, curr_y);
-    printf("\x1b[10;1H   Tokens spent = %d", tokens);
-    printf("\x1b[11;1H   Prizes caught = %d", prizes);
-    printf("\x1b[12;1H   No button pressed yet");
+    printf("\x1b[10;1H   Current (X, Y) = (%d, %d)", curr_x, curr_y);
+    printf("\x1b[11;1H   Tokens spent = %d", tokens);
+    printf("\x1b[12;1H   Prizes caught = %d", prizes);
+    printf("\x1b[13;1H   No button pressed yet");
 
     // Main loop
     while(appletMainLoop())
@@ -152,103 +195,161 @@ int main(int argc, char* argv[])
         u64 kDown = padGetButtonsDown(&pad);
 
         // padGetButtons returns the set of buttons that are currently pressed
-        u64 kHeld = padGetButtons(&pad);
+        //u64 kHeld = padGetButtons(&pad);
 
         // padGetButtonsUp returns the set of buttons that have been
         // newly released in this frame compared to the previous one
-        u64 kUp = padGetButtonsUp(&pad);
+        //u64 kUp = padGetButtonsUp(&pad);
 
         if (kDown & HidNpadButton_Plus)
             break; // break in order to return to hbmenu
 
         // Do the keys printing only if keys have changed
-        if (kDown != kDownOld || kHeld != kHeldOld || kUp != kUpOld)
-        {
-            // Clear console
-            consoleClear();
+        //if (kDown != kDownOld || kHeld != kHeldOld || kUp != kUpOld)
+        //{
+        // Clear console
+        consoleClear();
 
-            // Rewrite lines after clearing the whole screen
-            printf("\x1b[1;1H Advent of Code 2024 Day 13: Claw Contraption");
-            printf("\x1b[2;1H Instructions: Press A/B to move right/forward");
-            printf("\x1b[3;1H               Press X/Y to cancel A/B movements (no token refunds!)");
+        // Rewrite lines after clearing the whole screen
+        printf("\x1b[1;1H Advent of Code 2024 Day 13: Claw Contraption");
+        printf("\x1b[2;1H Instructions: Press A/B to move right/forward by some amount");
+        printf("\x1b[3;1H               Press X/Y to move left/backward by the same amount,");
+        printf("\x1b[4;1H                 but also costs tokens!");
 
-            printf("\x1b[4;1H   Machine %d of %d", machine_num, total_machines);
-            printf("\x1b[5;1H     Button A: X+%d, Y+%d", ax, ay);
-            printf("\x1b[6;1H     Button B: X+%d, Y+%d", bx, by);
-            printf("\x1b[7;1H     Prize: X=%d, Y=%d", px, py);
+        printf("\x1b[5;1H   Machine %d of %d", machine_num, total_machines);
+        printf("\x1b[6;1H     Button A: X+%d, Y+%d", ax, ay);
+        printf("\x1b[7;1H     Button B: X+%d, Y+%d", bx, by);
+        printf("\x1b[8;1H     Prize: X=%d, Y=%d", px, py);
 
-            printf("\x1b[9;1H   Current (X, Y) = (%d, %d)", curr_x, curr_y);
-            printf("\x1b[10;1H   Tokens spent = %d", tokens);
-            printf("\x1b[11;1H   Prizes caught = %d", prizes);
-            if (old_i > -1) {
-                printf("\x1b[12;1H   Button %s pressed, position updated: (%d, %d) -> (%d, %d)", keysNames[old_i], old_x, old_y, curr_x, curr_y);
-            }
-            
-            // Check if some of the keys are down, held or up
-            // Max index of 3 for A, B, X, Y
-            for (i = 0; i < 4; i++)
-            {
-                if (kDown & BIT(i)){
-                    //printf("%s down\n", keysNames[i]);
-
-                    old_i = i;
-                    old_x = curr_x;
-                    old_y = curr_y;
-                    if (i == 0) { // Button A pressed
-                        curr_x = curr_x + ax;
-                        curr_y = curr_y + ay;
-                        tokens = tokens + 3;
-                    }
-                    else if (i == 1) { // Button B pressed
-                        curr_x = curr_x + bx;
-                        curr_y = curr_y + by;
-                        tokens = tokens + 1;
-                    }
-                    else if (i == 2) { // Button X pressed
-                        curr_x = curr_x - ax;
-                        curr_y = curr_y - ay;
-                    }
-                    else if (i == 3) { // Button Y pressed
-                        curr_x = curr_x - bx;
-                        curr_y = curr_y - by;
-                    }
-
-                    // Only 1 button can be pressed at a time, so exit loop
-                    // This means button priorities are A, B, X, Y
-                    break;
-                }
-                //if (kHeld & BIT(i)) printf("%s held\n", keysNames[i]);
-                //if (kUp & BIT(i)) printf("%s up\n", keysNames[i]);
-            }
+        printf("\x1b[10;1H   Current (X, Y) = (%d, %d)", curr_x, curr_y);
+        printf("\x1b[11;1H   Tokens spent = %d", tokens);
+        printf("\x1b[12;1H   Prizes caught = %d", prizes);
+        if (old_i > -1) {
+            printf("\x1b[13;1H   Button %s pressed, position updated: (%d, %d) -> (%d, %d)", keysNames[old_i], old_x, old_y, curr_x, curr_y);
         }
+        
+        // Check if some of the keys are down, held or up
+        // Max index of 3 for A, B, X, Y
+        button_pressed = 0;
+        for (i = 0; i < 4; i++)
+        {
+            if (kDown & BIT(i)){
+                //printf("%s down\n", keysNames[i]);
+
+                old_i = i;
+                old_x = curr_x;
+                old_y = curr_y;
+                if (i == 0) { // Button A pressed
+                    curr_x = curr_x + ax;
+                    curr_y = curr_y + ay;
+                    tokens = tokens + 3;
+                }
+                else if (i == 1) { // Button B pressed
+                    curr_x = curr_x + bx;
+                    curr_y = curr_y + by;
+                    tokens = tokens + 1;
+                }
+                else if (i == 2) { // Button X pressed
+                    curr_x = curr_x - ax;
+                    curr_y = curr_y - ay;
+                }
+                else if (i == 3) { // Button Y pressed
+                    curr_x = curr_x - bx;
+                    curr_y = curr_y - by;
+                }
+
+                // Only 1 button can be pressed at a time, so exit loop
+                // This means button priorities are A, B, X, Y
+                button_pressed = 1;
+                break;
+            }
+            //if (kHeld & BIT(i)) printf("%s held\n", keysNames[i]);
+            //if (kUp & BIT(i)) printf("%s up\n", keysNames[i]);
+        }
+        //}
 
         // Check current position vs prize position
         if ((curr_x > px) || (curr_y > py)) {
             // Overshot and did not get the prize
-            printf("\x1b[13;1H   Went beyond the prize! Go back with X/Y!");
+            printf("\x1b[14;1H   Went beyond the prize! Go back with X/Y!");
+            prize_caught = 0;
         }
         else if ((curr_x == px) && (curr_y == py)) {
             // Got the prize
-            printf("\x1b[13;1H   Congratulations! You caught a prize!");
-            prizes = prizes + 1;
+            printf("\x1b[14;1H   Congratulations! You caught a prize!");
+            if (!prize_caught) {
+                prizes = prizes + 1;
+                prize_caught = 1;
+            }
 
             // TODO: Print option to go to next machine
         }
+        else {
+            prize_caught = 0;
+        }
 
-        // Print grid lines
         // TODO: handle negative coordinates
         grid_x_offset = curr_x % 10;
         grid_y_offset = curr_y % 10;
-        for (i = 0 ; i < GRID_LINES_ROWS; i++) {
-            printf("\x1b[%d;%dH", INIT_ROW_GRID_LINES+i, INIT_COL_GRID_LINES);
-            snprintf(sliced_grid_line, GRID_LINES_COLS+1, "%s", grid_lines[i+grid_x_offset]+grid_y_offset);
-            printf("%s", sliced_grid_line);
+        if (button_pressed) {
+
+            // Print grid lines
+            // TODO: Implement Bresenham's algorithm for straight line animation?
+            //for (x = 1; x < grid_x_offset+1; x++) {
+            //    for (y = 1; y < grid_y_offset+1; y++) {
+            //        // Add delay
+            //        wait_delay();
+            //
+            //        for (i = 0 ; i < GRID_LINES_ROWS; i++) {
+            //            printf("\x1b[%d;%dH", INIT_ROW_GRID_LINES+i, INIT_COL_GRID_LINES);
+            //            snprintf(sliced_grid_line, GRID_LINES_COLS+1, "%s", grid_lines[i+x]+y);
+            //            printf("%s", sliced_grid_line);
+            //        }
+            //    }
+            //}
+            // Horizontal scroll
+            for (x = 1; x < grid_x_offset+1; x++) {
+                wait_delay();
+
+                for (i = 0 ; i < GRID_LINES_ROWS; i++) {
+                    printf("\x1b[%d;%dH", INIT_ROW_GRID_LINES+i, INIT_COL_GRID_LINES);
+                    snprintf(sliced_grid_line, GRID_LINES_COLS+1, "%s", grid_lines[ i + 10] + x);
+                    printf("%s", sliced_grid_line);
+                }
+            }
+            // Vertical scroll
+            for (y = 1; y < grid_y_offset+1; y++) {
+                wait_delay();
+
+                for (i = 0 ; i < GRID_LINES_ROWS; i++) {
+                    printf("\x1b[%d;%dH", INIT_ROW_GRID_LINES+i, INIT_COL_GRID_LINES);
+                    snprintf(sliced_grid_line, GRID_LINES_COLS+1, "%s", grid_lines[ i + 10 - y] + grid_x_offset);
+                    printf("%s", sliced_grid_line);
+                }
+            }
+        }
+        else {
+            // Still print grid lines when no button is pressed since the screen was cleared,
+            // but print with no delay
+            for (i = 0 ; i < GRID_LINES_ROWS; i++) {
+                printf("\x1b[%d;%dH", INIT_ROW_GRID_LINES+i, INIT_COL_GRID_LINES);
+                snprintf(sliced_grid_line, GRID_LINES_COLS+1, "%s", grid_lines[ i + 10 - grid_y_offset] + grid_x_offset);
+                printf("%s", sliced_grid_line);
+            }
         }
 
         // Print claw
         printf("\x1b[%d;%dH%s", CLAW_ROW-1, CLAW_COL-1, CLAW_TOP);
         printf("\x1b[%d;%dH%s", CLAW_ROW, CLAW_COL, CLAW_MID);
         printf("\x1b[%d;%dH%s", CLAW_ROW+1, CLAW_COL-1, CLAW_BOT);
+
+        // Print prize if within grid
+        diff_x = curr_x - px;
+        diff_y = curr_y - py;
+        if ((abs(diff_x) < ((GRID_LINES_ROWS-1)/2)) &&
+            (abs(diff_y) < ((GRID_LINES_COLS-1)/2))) {
+            printf("\x1b[%d;%dH%s", CLAW_ROW+diff_x, CLAW_COL+diff_y-1, PRIZE_MID);
+        }
 
         // Print horizontal borders
         // Upper left corner
@@ -280,8 +381,8 @@ int main(int argc, char* argv[])
 
         // Set keys old values for the next frame
         kDownOld = kDown;
-        kHeldOld = kHeld;
-        kUpOld = kUp;
+        //kHeldOld = kHeld;
+        //kUpOld = kUp;
 
         // Update the console, sending a new frame to the display
         consoleUpdate(NULL);
