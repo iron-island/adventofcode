63c22effa648c43eade2f845a35ebac46e3b8c01
iron-island <ironislandmail@gmail.com>
2025-01-04 13:13:41 +0800

    Retroactive solution to 2021 day 25

--- solutions/2021/25.py
+++ solutions/2021/25.py
@@ -1,18 +1,99 @@
+import copy
+from collections import defaultdict
+
 input_file = "../../inputs/2021/input25.txt"
 example_file = "example25.txt"
 
 part1_example = 0
 part1 = 0
+
+def print_grid(grid_dict, MAX_ROW, MAX_COL):
+    for row in range(0, MAX_ROW+1):
+        for col in range(0, MAX_COL+1):
+            print(grid_dict[(row, col)], end="")
+        print("")
+
 def process_inputs(in_file):
     output = 0
 
+    grid_dict = defaultdict(str)
+    MAX_ROW = 0
+    MAX_COL = 0
     with open(in_file) as file:
         line = file.readline()
-    
+   
+        row = 0 
         while line:
             line = line.strip()
 
+            col = 0
+            for tile in line:
+                grid_dict[(row, col)] = tile
+                col += 1
+            MAX_COL = col-1
+
+            row += 1
+
             line = file.readline()
+        MAX_ROW = row-1
+
+    # Debugging
+    print("Initial state")
+    print_grid(grid_dict, MAX_ROW, MAX_COL)
+    print("")
+
+    steps = 0
+    while True:
+        no_change = True
+        steps += 1
+        print(f'Step {steps}')
+
+        #if (steps == 5):
+        #    break
+
+        # Move east-facing first, starting from MAX_COL
+        new_grid_dict = copy.deepcopy(grid_dict)
+        for col in range(MAX_COL, -1, -1):
+            for row in range(0, MAX_ROW+1):
+                if (grid_dict[(row, col)] != ">"):
+                    continue
+
+                if (col < MAX_COL):
+                    n_rc = (row, col+1)
+                else:
+                    n_rc = (row, 0)
+
+                if (grid_dict[n_rc] == "."):
+                    new_grid_dict[(row, col)] = "."
+                    new_grid_dict[n_rc] = ">"
+                    no_change = False
+
+        # Move south-facing second, starting from 0
+        grid_dict = copy.deepcopy(new_grid_dict)
+        for row in range(0, MAX_ROW+1):
+            for col in range(0, MAX_COL+1):
+                if (new_grid_dict[(row, col)] != "v"):
+                    continue
+
+                if (row < MAX_ROW):
+                    n_rc = (row+1, col)
+                else:
+                    n_rc = (0, col)
+
+                if (new_grid_dict[n_rc] == "."):
+                    grid_dict[(row, col)] = "."
+                    grid_dict[n_rc] = "v"
+                    no_change = False
+
+        # Check if there is no change
+        if (no_change):
+            break
+
+        # DEBUGGING
+        #print_grid(grid_dict, MAX_ROW, MAX_COL)
+        #print("")
+
+    output = steps
 
     return output
 
