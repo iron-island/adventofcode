95c04df19464bd21976a729b8d0ebd1f2613b6dc
iron-island <ironislandmail@gmail.com>
2025-01-06 00:19:25 +0800

    Further runtime optimization for 2024 day 7 by removing the DFS cache, correctly passing an initial number instead of 0, and removing some dead code

--- solutions/2024/07.py
+++ solutions/2024/07.py
@@ -1,6 +1,3 @@
-import numpy as np
-from functools import cache
-
 input_file = "../../inputs/2024/input07.txt"
 example_file = "example07.txt"
 
@@ -14,39 +11,44 @@ OPS2 = ['+', '*', '|']
 def concat(x, y):
     return x + y
 
-@cache
+# Note: Adding a cache slows down the DFS, since this is already fast
 def dfs(eq, num_tuple, result):
     # Early exit if current test result is already larger than expected
     if (eq > result):
         return None
 
-    # Base case
+    # Base case since there are no more numbers to operate on
     if (len(num_tuple) == 0):
         if (eq == result):
             return eq
         else:
             return None
 
-    num_str = num_tuple[0]
+    # Get next number to operate on, while remaining numbers
+    #   in num_tuple[1:] will be used on further calls
+    num = num_tuple[0]
+    next_num_tuple = num_tuple[1:]
+
     # Addition
-    eq1 = dfs(eq + int(num_str), num_tuple[1:], result)
+    eq1 = dfs(eq + num, next_num_tuple, result)
 
     if (eq1 == result):
         return result
 
     # Multiplication
-    eq2 = dfs(eq*int(num_str), num_tuple[1:], result)
+    eq2 = dfs(eq*num, next_num_tuple, result)
 
     if (eq2 == result):
         return result
 
     # Concatenation
-    eq3 = dfs(eq*(10**len(num_str)) + int(num_str), num_tuple[1:], result)
+    eq3 = dfs(eq*(10**len(str(num))) + num, next_num_tuple, result)
 
     if (eq3 == result):
         return result
 
     # If did not become equal to result, return None so it fails test equality
+    # Python "returns" this by default, but putting here just to be more explicit
     return None
 
 def process_inputs(in_file):
@@ -129,8 +131,8 @@ def process_inputs2(in_file):
             line = line.strip()
 
             result, nums = line.split(": ")
-            num_list = nums.split()
-            eq_list.append([result, num_list])
+            num_tuple = tuple([int(x) for x in nums.split()])
+            eq_list.append([int(result), num_tuple])
 
             line = file.readline()
 
@@ -138,25 +140,26 @@ def process_inputs2(in_file):
     count = 0
     true_result = 0
     eq_count = 1
+    MAX_EQ_COUNT = len(eq_list)
     for eq in eq_list:
-        print(f'{eq_count} of {len(eq_list)}')
+        print(f'{eq_count} of {MAX_EQ_COUNT}')
         eq_count += 1
-        result, num_list = eq
-        num_operators = len(num_list)-1
-        op_list = []
-        num_comb = (3**num_operators)
+        result, num_tuple = eq
 
-        # Optimal DFS
-        eq_int = dfs(0, tuple(num_list), int(result))
+        # Brute-force via DFS with no memoization
+        test_result = dfs(num_tuple[0], num_tuple[1:], result)
 
         # Evaluate
-        test_result = eq_int
-        if (int(result) == test_result):
+        if (result == test_result):
             count += 1
             true_result += test_result
         continue
 
         # Former brute-force linear search solution which was slower
+        #result, num_list = eq
+        #num_operators = len(num_list)-1
+        #op_list = []
+        #num_comb = (3**num_operators)
         #for i in range(0, num_comb):
         #    # Convert to base 3 string
         #    #bin_string = bin(i)
