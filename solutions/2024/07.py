03185872d2a7f4782c8f080412a1ef4a18ecedb8
iron-island <ironislandmail@gmail.com>
2025-01-05 23:45:40 +0800

    Further runtime optimization for 2024 day 7 by changing to DFS with memoization and early exit conditions

--- solutions/2024/07.py
+++ solutions/2024/07.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functools import cache
 
 input_file = "../../inputs/2024/input07.txt"
 example_file = "example07.txt"
@@ -12,6 +13,41 @@ OPS2 = ['+', '*', '|']
 
 def concat(x, y):
     return x + y
+
+@cache
+def dfs(eq, num_tuple, result):
+    # Early exit if current test result is already larger than expected
+    if (eq > result):
+        return None
+
+    # Base case
+    if (len(num_tuple) == 0):
+        if (eq == result):
+            return eq
+        else:
+            return None
+
+    num_str = num_tuple[0]
+    # Addition
+    eq1 = dfs(eq + int(num_str), num_tuple[1:], result)
+
+    if (eq1 == result):
+        return result
+
+    # Multiplication
+    eq2 = dfs(eq*int(num_str), num_tuple[1:], result)
+
+    if (eq2 == result):
+        return result
+
+    # Concatenation
+    eq3 = dfs(eq*(10**len(num_str)) + int(num_str), num_tuple[1:], result)
+
+    if (eq3 == result):
+        return result
+
+    # If did not become equal to result, return None so it fails test equality
+    return None
 
 def process_inputs(in_file):
     output = 0
@@ -109,54 +145,66 @@ def process_inputs2(in_file):
         num_operators = len(num_list)-1
         op_list = []
         num_comb = (3**num_operators)
-        for i in range(0, num_comb):
-            # Convert to base 3 string
-            #bin_string = bin(i)
-            #bin_string = bin_string[2:]
-            bin_string = np.base_repr(int(i), base=3)
 
-            # Left pad
-            if (len(bin_string) < num_operators):
-                num_pad = num_operators - len(bin_string)
-                for n in range(0, num_pad):
-                    bin_string = "0" + bin_string
+        # Optimal DFS
+        eq_int = dfs(0, tuple(num_list), int(result))
 
-            # Permutations
-            #eq = num_list[0]
-            eq_int = int(num_list[0])
-            int_result = int(result)
-            for idx, b in enumerate(bin_string):
-                # Original solution, slow
-                #op = OPS2[int(b)]
+        # Evaluate
+        test_result = eq_int
+        if (int(result) == test_result):
+            count += 1
+            true_result += test_result
+        continue
 
-                #if (op != '|'):
-                #    eq = eq + op + num_list[idx+1]
-                #    eq = str(eval(eq))
-                #else:
-                #    eq = eq + num_list[idx+1]
+        # Former brute-force linear search solution which was slower
+        #for i in range(0, num_comb):
+        #    # Convert to base 3 string
+        #    #bin_string = bin(i)
+        #    #bin_string = bin_string[2:]
+        #    bin_string = np.base_repr(int(i), base=3)
 
-                # Optimized solution
-                if (eq_int > int_result):
-                    # Early exit if current test result is already larger than expected
-                    break
-                num_str = num_list[idx+1]
-                if (b == "0"):
-                    # Addition
-                    eq_int = eq_int + int(num_str)
-                elif (b == "1"):
-                    # Multiplication
-                    eq_int = eq_int*int(num_str)
-                elif (b == "2"):
-                    # Concatenation
-                    eq_int = eq_int*(10**len(num_str)) + int(num_str)
+        #    # Left pad
+        #    if (len(bin_string) < num_operators):
+        #        num_pad = num_operators - len(bin_string)
+        #        for n in range(0, num_pad):
+        #            bin_string = "0" + bin_string
 
-            # Evaluate
-            #test_result = eval(eq)
-            test_result = eq_int
-            if (int(result) == test_result):
-                count += 1
-                true_result += test_result
-                break
+        #    # Permutations
+        #    #eq = num_list[0]
+        #    eq_int = int(num_list[0])
+        #    int_result = int(result)
+        #    for idx, b in enumerate(bin_string):
+        #        # Original solution, slow
+        #        #op = OPS2[int(b)]
+
+        #        #if (op != '|'):
+        #        #    eq = eq + op + num_list[idx+1]
+        #        #    eq = str(eval(eq))
+        #        #else:
+        #        #    eq = eq + num_list[idx+1]
+
+        #        # Optimized solution
+        #        if (eq_int > int_result):
+        #            # Early exit if current test result is already larger than expected
+        #            break
+        #        num_str = num_list[idx+1]
+        #        if (b == "0"):
+        #            # Addition
+        #            eq_int = eq_int + int(num_str)
+        #        elif (b == "1"):
+        #            # Multiplication
+        #            eq_int = eq_int*int(num_str)
+        #        elif (b == "2"):
+        #            # Concatenation
+        #            eq_int = eq_int*(10**len(num_str)) + int(num_str)
+
+        #    # Evaluate
+        #    #test_result = eval(eq)
+        #    test_result = eq_int
+        #    if (int(result) == test_result):
+        #        count += 1
+        #        true_result += test_result
+        #        break
 
     print(count)
     output = true_result
