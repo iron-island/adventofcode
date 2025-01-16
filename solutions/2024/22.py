1d8933e57ed516f49a4eae109829bd59e48cd650
iron-island <154013605+iron-island@users.noreply.github.com>
2025-01-17 00:04:53 +0800

    Minor clean-up for 2024 day 22 on some comments and removed outdated code

--- solutions/2024/22.py
+++ solutions/2024/22.py
@@ -19,8 +19,6 @@ def part1_part2(in_file):
     #   so reverted them to lists
     # Comment: https://www.reddit.com/r/adventofcode/comments/1hjroap/comment/m3cdba8/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
     # Repo: https://github.com/APMorto/aoc2024/blob/master/day_22_monkey_market/monkey_market.py
-    #banana_array = array('H', [0]*ARRAY_SIZE)
-    #seq_array = array('H', [0]*ARRAY_SIZE)
 
     # List length = 18*(19**3 + 19**2 + 19 + 1)
     #               18 comes from the number of possible changes
@@ -34,49 +32,38 @@ def part1_part2(in_file):
         next_idx_s = idx_s+1
         new_secret = init_secret
         for i in range(0, 2000):
-            # Original bitwise operations
-            #new_secret = (new_secret^(new_secret <<  6)) & 16777215
-            #new_secret = (new_secret^(new_secret >>  5)) & 16777215
-            #new_secret = (new_secret^(new_secret << 11)) & 16777215
-
             # Optimize bitwise operations such that there is no single operation result that has more than 24 bits,
             #   and implement shifts as multiplication/integer division:
-            #   1. Bit mask by (24-6) = 18 bits first before left shifting and XORing
+            #   1. n = (n^(n <<  6)) & 0xFFFFFF
+            #      Bit mask by (24-6) = 18 bits first before left shifting and XORing
             #      Bit mask as bitwise operation seems to be marginally faster than modulo for this value
-            #   2. No need to bit mask since right shifting ensures that it is always 24 bits
-            #   3. Bit mask by (24-11) = 13 bits first before left shifting and XORing
+            #   2. n = (n^(n <<  5)) & 0xFFFFFF
+            #      No need to bit mask since right shifting ensures that it is always 24 bits
+            #   3. n = (n^(n << 11)) & 0xFFFFFF
+            #      Bit mask by (24-11) = 13 bits first before left shifting and XORing
             #      Bit mask implemented as modulo seems to be marginally faster for this value
             new_secret = new_secret^((new_secret & 0b111111111111111111) * 64)
             new_secret = new_secret^(new_secret // 32)
             new_secret = new_secret^((new_secret % 8192) * 2048)
 
-            # TODO: Numbers repeat so we can skip some iterations?
-            #if (new_secret in init_set):
-            #    num_repeat += 1
-            #    print(f'{new_secret} with index {i}  already in init_list! {num_repeat}')
-
             # Instead of shifting the sequence of changes which we end up just using
             #   as an index, we instead compute the index straight away, and shifting
             #   is done by simply dividing the previous index by 19, and then adding
             #   the new change scaled by 19**3
-            # This is equivalent to the following:
-            #   changes_list[0] = changes_list[1]
-            #   changes_list[1] = changes_list[2]
-            #   changes_list[2] = changes_list[3]
-            #   changes_list[3] = change
-            #   idx_array = changes_list[3]*(19**3) + \
-            #               changes_list[2]*(19**2) + \
-            #               changes_list[1]*19 + \
-            #               changes_list[0]
+            # This is equivalent to the following, but without needing to track changes:
+            #   changes_list[0:4] = changes_list[1:4] + [change]
+            #   idx_change_seq = changes_list[3]*(19**3) + \
+            #                    changes_list[2]*(19**2) + \
+            #                    changes_list[1]*19 + \
+            #                    changes_list[0]
             d = (new_secret % 10)
             if (i == 0):
-                # change = 0
+                # change = 0, so set index to 0
                 idx_change_seq = 0
             else:
                 # Offset by 9 since change is -9 to +9, still works
                 #   without offsets due to negative indices but is slower
-                # change = (d - prev_d) + 9
-                # Scale by 19**3 = 6859
+                # change = (d - prev_d) + 9 then scale by 19**3 = 6859
                 idx_change_seq = ((d - prev_d)+9)*(6859) + (prev_idx_change_seq//19)
             prev_d = d
             prev_idx_change_seq = idx_change_seq
