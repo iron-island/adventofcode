d42f9816938e55497688a9b2a970d937b756582c
iron-island <154013605+iron-island@users.noreply.github.com>
2025-01-18 13:31:27 +0800

    Clean-up of 2024 day 20 by simplifying path traversal

--- solutions/2024/20.py
+++ solutions/2024/20.py
@@ -9,125 +9,82 @@ from collections import defaultdict
 
 input_file = "../../inputs/2024/input20.txt"
 
-def get_dist(tuple1, tuple2):
-    row1, col1 = tuple1
-    row2, col2 = tuple2
-
-    diff_row = abs(row2-row1)
-    diff_col = abs(col2-col1)
-
-    return diff_row+diff_col
-
 def part1_part2(in_file):
-    grid_dict = defaultdict(str)
-    node_dict = defaultdict(list)
+    grid = []
     with open(in_file) as file:
         line = file.readline()
 
-        row = 0    
+        row = 0
         while line:
             line = line.strip()
 
-            for col, l in enumerate(line):
-                grid_dict[(row, col)] = l
-                node_dict[(row, col)] = [10000000, None]
+            grid.append(line)
 
-                if (l == "S"):
-                    S_pos = (row, col)
-                elif (l == "E"):
-                    E_pos = (row, col)
-            MAX_COL = len(line)-1
+            # Check start and end coordinates
+            if ("S" in line):
+                start_rc = (row, line.index("S"))
+            elif ("E" in line):
+                end_rc = (row, line.index("E"))
 
             row += 1
-
             line = file.readline()
 
-    MAX_ROW = row-1
-    #print(MAX_ROW, MAX_COL)
+    MAX_ROW = len(grid)-1
+    MAX_COL = len(grid[0])-1
 
-    # Initial BFS
-    q = []
-    visited = set()
-    #idx_dict = defaultdict(int)
-    t = 0
-    row, col = S_pos
-    dist = get_dist(S_pos, E_pos)
-    q.append((t+dist, row, col, t))
-    node_dict[S_pos] = [t, None]
+    # Traverse the path
+    # Heuristic: There is only 1 path so BFS is enough
+    #            and there's no need to use a set of
+    #            visited coordinates
+    rc = start_rc
+    prev_rc = None
+
+    best_path = []
     idx = 0
-    while (len(q)):
-        # Priority queue
-        q.sort()
-        heuristic, row, col, t = q.pop(0)
+    idx_dict = defaultdict(int)
+    while True:
+        best_path.append(rc)
+        idx_dict[rc] = idx
+        idx += 1
 
-        if ((row, col) == E_pos):
+        # If we arrived at the end, add as a turn
+        if (rc == end_rc):
             break
 
-        if ((row, col) in visited):
-            continue
-        visited.add((row, col))
+        # Check which direction is the next coordinate
+        row, col = rc
 
-        # up
+        # Up
         n_row = row-1
-        n_col = col
-        n_tuple = (n_row, n_col)
-        dist = get_dist(n_tuple, E_pos)
-        if (n_tuple in grid_dict) and (grid_dict[n_tuple] != '#'):
-            prev_t, prev_node = node_dict[n_tuple]
-            if (prev_t >= (t+1)):
-                q.append((t+1+dist, n_row, n_col, t+1))
-                node_dict[n_tuple] = [t+1, (row, col)]
-
-        # down
+        next_rc = (n_row, col)
+        if (grid[n_row][col] != "#") and (next_rc != prev_rc):
+            prev_rc = rc
+            rc = next_rc
+            continue
+        
+        # Down
         n_row = row+1
-        n_col = col
-        n_tuple = (n_row, n_col)
-        dist = get_dist(n_tuple, E_pos)
-        if (n_tuple in grid_dict) and (grid_dict[n_tuple] != '#'):
-            prev_t, prev_node = node_dict[n_tuple]
-            if (prev_t >= (t+1)):
-                q.append((t+1+dist, n_row, n_col, t+1))
-                node_dict[n_tuple] = [t+1, (row, col)]
+        next_rc = (n_row, col)
+        if (grid[n_row][col] != "#") and (next_rc != prev_rc):
+            prev_rc = rc
+            rc = next_rc
+            continue
 
-        # left
-        n_row = row
+        # Left
         n_col = col-1
-        n_tuple = (n_row, n_col)
-        dist = get_dist(n_tuple, E_pos)
-        if (n_tuple in grid_dict) and (grid_dict[n_tuple] != '#'):
-            prev_t, prev_node = node_dict[n_tuple]
-            if (prev_t >= (t+1)):
-                q.append((t+1+dist, n_row, n_col, t+1))
-                node_dict[n_tuple] = [t+1, (row, col)]
+        next_rc = (row, n_col)
+        if (grid[row][n_col] != "#") and (next_rc != prev_rc):
+            prev_rc = rc
+            rc = next_rc
+            continue
 
-        # right
-        n_row = row
+        # Right
         n_col = col+1
-        n_tuple = (n_row, n_col)
-        dist = get_dist(n_tuple, E_pos)
-        if (n_tuple in grid_dict) and (grid_dict[n_tuple] != '#'):
-            prev_t, prev_node = node_dict[n_tuple]
-            if (prev_t >= (t+1)):
-                q.append((t+1+dist, n_row, n_col, t+1))
-                node_dict[n_tuple] = [t+1, (row, col)]
-
-    max_t = t
-
-    # Alternative 3: WORKS
-    # Reconstruct best path without cheats
-    prev_t, prev_node = node_dict[E_pos]
-    best_path = []
-    best_path.append(E_pos)
-    while (prev_node != None):
-        best_path.append(prev_node)
-
-        prev_t, prev_node = node_dict[prev_node]
-    best_path.reverse()
-
-    # Convert to dict
-    idx_dict = defaultdict(int)
-    for idx, rc in enumerate(best_path):
-        idx_dict[rc] = idx
+        next_rc = (row, n_col)
+        if (grid[row][n_col] != "#") and (next_rc != prev_rc):
+            prev_rc = rc
+            rc = next_rc
+            continue
 
     # Precompute the row offset, column offset, and distance
     offset_list = [(r, c, abs(r)+abs(c)) for r in range(-20, 21) for c in range(abs(r)-20, (20-abs(r))+1)]
